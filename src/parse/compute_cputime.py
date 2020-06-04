07ae04fb66fef83e09e6482eec325c76c10a1727
Nuutti Sten <nuutti.sten@aalto.fi>
2020-06-04 14:43:35 +0300

    computing cpu time for baselines works now

--- src/parse/compute_cputime.py
+++ src/parse/compute_cputime.py
@@ -31,7 +31,7 @@ def compute_cputime(experiment, exptype):
     Function to read parsed boss.out json files, compute single core cpu time 
     approximations to those and save the results in the same file.
     """
-    path = os.path.expanduser(config['path'])
+    path = os.path.expanduser(str(experiment['path']))
     if exptype == 'single_output':
         for name, single_core_time in zip(experiment['names'], experiment['single_core_time']):
             data = None
@@ -40,7 +40,7 @@ def compute_cputime(experiment, exptype):
                 
             if data is not None: # write to file
                 N = len(data['acqtime'])
-                data['modeltime'] = [itertime-acqtime for itertime, acqtime in zip(data['itertime'], data['acqtime'])
+                data['modeltime'] = [itertime-acqtime for itertime, acqtime in zip(data['itertime'], data['acqtime'])]
                 data['cputime'] = [sum(data['modeltime'][:i])+(i+1)*single_core_time for i in range(N)]
                 with open(f'{path}{name}.json', 'w') as file:
                     json.dump(data, file)
@@ -49,13 +49,13 @@ def compute_cputime(experiment, exptype):
         name = experiment['namebase']
         for i in range(1,experiment['N_experiments']+1):
             data = None
-            with with open(f'{path}{name}{i}.json', 'r') as file:
+            with open(f'{path}{name}{i}.json', 'r') as file:
                 data = json.load(file)
             if data is not None:
                 initpts_cputimes = []
                 initpts_list = data['initpts']
                 # load cpu times for initpts
-                for baseline, initpts in zip(experiment['baselines'], initpts_list)
+                for baseline, initpts in zip(experiment['baselines'], initpts_list):
                     with open(f'{path}{baseline}','r') as file:
                         data = json.load(file)
                         if 'cputime' in data:
@@ -72,8 +72,9 @@ def compute_cputime(experiment, exptype):
                         data['cputime'].append(cputime)
                 init_cputime_sum = data['cputime'][-1]
                 # Compute cpu times for experiments           
+                single_core_time = experiment['single_core_time']
                 N = len(data['acqtime'])
-                data['modeltime'] = [itertime-acqtime for itertime, acqtime in zip(data['itertime'][-N:], data['acqtime'])
+                data['modeltime'] = [itertime-acqtime for itertime, acqtime in zip(data['itertime'][-N:], data['acqtime'])]
                 for i in range(N):
                     data['cputime'].append(sum(data['modeltime'][:i])+(i+1)*single_core_time + init_cputime_sum)
                 
@@ -91,7 +92,7 @@ def main(config):
     for exptype in types:
         if exptype in config:
             for experiment in config[exptype]:
-                compute_cputime(experiment, exptype):
+                compute_cputime(experiment, exptype)
                     
 
 
@@ -100,5 +101,5 @@ if __name__=='__main__':
     args = sys.argv
     configfile = args[1]
     with open(configfile, 'r') as file:
-        config = yaml.load(file)
-        
+        config = yaml.load(file, Loader=yaml.FullLoader)
+        main(config)
---
+++ src/test/test_compute_cputime.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# test computing cpu times for experiments
+
+python3 ../parse/compute_cputime.py config/test_compute_cputime.yaml
