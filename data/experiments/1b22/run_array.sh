3ccdc06618c20ff2cd1d8e6198ebeb5ac8f3bb2c
Nuutti Sten <nuutti.sten@aalto.fi>
2020-07-13 08:44:23 +0300

    all raw data is now under data/experiments with coded names. a list of experiment descriptions must be provided

---
+++ data/experiments/1b22/run_array.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+### run an array job of experiments
+
+#BATCH --job-name=1b22
+#SBATCH --account=project_2000382
+#SBATCH --array=1-30
+#SBATCH --partition=small
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=1
+#SBATCH -o job/job%A_%a.out
+#SBATCH -e job/job%A_%a.err
+#####SBATCH --mem-per-cpu=2G
+
+scp -r template exp_${SLURM_ARRAY_TASK_ID}
+cd exp_${SLURM_ARRAY_TASK_ID}
+./run_BOSS.sh
+cd ..
