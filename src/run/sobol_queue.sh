24a479bf460b3cd3c8e0908f64eded3155205ff8
Nuutti Sten <nuutti.sten@aalto.fi>
2020-11-16 13:10:49 +0200

    removed old analysis scripts etc

--- src/run/sobol_queue.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cwd=$pwd
-cd ~/BOSS-MT/src
-python3 parse/parse_files.py config/parse_sobol_queue.yaml
-python3 parse/preprocess.py config/preprocess_sobol_queue.yaml
-cd $cwd  
--- src/test/test_parse.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-### this scipt runs parse_files.py script
-python3 ~/BOSS-MT/src/parse/parse_files.py config/test_parse.yaml
